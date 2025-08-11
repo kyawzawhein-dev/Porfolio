91883ab9a1a02c986c4dd4800f632598ba765a0d
kyawzawhein-dev <kyawzawhein869@gmail.com>
2025-08-12 04:45:04 +0700

    fix: about me and footer

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"
 import "./index.css";
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Footer />
-    </div>
+<>
+<Header />
+<Footer />
+</>
   );
 }
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,17 +1,24 @@
-import React from "react";
-import profileImg from "../assets/profile.jpg"; // replace with your image
+import profile from "../assets/profile.jpg";
+import "../css/header.css";
 
-export default function Header() {
+export default function Header(){
   return (
     <header className="header">
-      <img src={profileImg} alt="Profile" className="profile-img" />
-      <div className="header-info">
-        <p><strong>Name:</strong> Kyaw Zaw Hein / Bo Bo / Jason</p>
-        <p><strong>Age:</strong> 22</p>
-        <p><strong>Ph No:</strong> 09963073590 (TH)</p>
-        <p><strong>Email:</strong> zawk9340@gmail.com</p>
-        <p><strong>Address:</strong> House No 334/85 Moo 4, San Phak Wan Subdistrict, Hang Dong district, Chiang Mai Province, Thailand</p>
-      </div>
+      {/* <div className="tagline">
+        <h4>Software Programmer</h4>
+        <p>Hard to find</p>
+        <p>Easy to lose</p>
+        <p>Impossible to forget</p>
+      </div> */}
+      <img
+        src={profile}
+        alt="Profile"
+        className="profile-img"
+      />
+      <h2>Kyaw Zaw Hein / Bo Bo / Jason</h2>
+      <h2>22</h2>
+      <h2>+66996321434 (TH)</h2>
+      <h2>zawk9340@gmail.com</h2>
     </header>
-  );
+  )
 }
